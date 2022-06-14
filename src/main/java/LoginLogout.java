ee865ef3860056e69011a22ec48729f4d4686a88
reinnatan <reinnatan@gmail.com>
2022-06-14 10:08:37 +0700

    modify home page

---
+++ src/main/java/LoginLogout.java
@@ -0,0 +1,26 @@
+
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class LoginLogout extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String email = req.getParameter("email");
+        String password = req.getParameter("password");
+        if (email.equals("admin@gmail.com") && password.equals("admin")) {
+            resp.sendRedirect("home.jsp");
+        }else{
+            resp.sendRedirect("index.jsp");
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.sendRedirect("index.jsp");
+    }
+}
