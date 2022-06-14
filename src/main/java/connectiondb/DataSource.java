ee3707123129c0eb14c02e4aff95787d5469aa1b
reinnatan <reinnatan@gmail.com>
2022-06-14 10:57:51 +0700

    add library datasource

---
+++ src/main/java/connectiondb/DataSource.java
@@ -0,0 +1,14 @@
+package connectiondb;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+//https://www.baeldung.com/hikaricp
+public class DataSource {
+    private static HikariConfig config = new HikariConfig();
+    private static HikariDataSource ds;
+
+    static {
+
+    }
+}
